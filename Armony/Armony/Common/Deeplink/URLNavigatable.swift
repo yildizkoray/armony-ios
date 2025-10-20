bad2830785e41ce6e9c6d244036d5f6cd9ab46ff
yildizkoray <koray.yildizz@gmail.com>
2025-10-20 21:31:01 +0300

    Remove deeplink in URLNavigatable

--- Armony/Armony/Common/Deeplink/URLNavigatable.swift
+++ Armony/Armony/Common/Deeplink/URLNavigatable.swift
@@ -2,15 +2,8 @@ import Foundation
 
 public protocol URLNavigatable {
     var isAuthenticationRequired: Bool { get}
-    static var deeplink: Deeplink { get }
 
     static var instance: URLNavigatable { get }
 
     static func register(navigator: URLNavigation)
-}
-
-extension URLNavigatable {
-    static var deeplink: Deeplink {
-        .accountInformation
-    }
 }
